74ff516f996c33547b75c4385eeaa6398169d1a1
kikimot145 <kikivault@gmail.com>
2021-02-10 20:21:40 +0300

    practice 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 
-const category = 'toys';
-console.log(`https://lol/${category}/5`);
+const numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const firstFilm = prompt('Один из последних просмотренных фильмов?', ''),
+      firstScore = prompt('На сколько оцените его?', ''),
+      secondFilm = prompt('Один из последних просмотренных фильмов?', ''),
+      secondScore = prompt('На сколько оцените его?', '');
+
+let movies = {};
+personalMovieDB.movies[firstFilm] = firstScore;
+personalMovieDB.movies[secondFilm] = secondScore;
+
+console.log(personalMovieDB);
