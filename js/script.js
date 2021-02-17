87d3b9267d9c029a625ca78128baef5ffa39dcfc
kikimot145 <kikivault@gmail.com>
2021-02-18 00:59:45 +0300

    Add function showMyDB

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,13 +20,41 @@ let personalMovieDB = {
     private: false
 };
 
-const firstFilm = prompt('Один из последних просмотренных фильмов?', ''),
-      firstScore = prompt('На сколько оцените его?', ''),
-      secondFilm = prompt('Один из последних просмотренных фильмов?', ''),
-      secondScore = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const film = prompt('Один из последних просмотренных фильмов?', ''),
+              score = prompt('На сколько оцените его?', '');
+    
+        if (film != null && score != null && film != '' && score != '' && film.length < 50 && score.length < 50) {
+            personalMovieDB.movies[film] = score;
+            console.log('Отлично');
+        } else {
+            console.log('Ошибка');
+            --i;
+        }    
+    }
+}
 
-let movies = {};
-personalMovieDB.movies[firstFilm] = firstScore;
-personalMovieDB.movies[secondFilm] = secondScore;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+    if (personalMovieDB.private) {
+        return;
+    }
+    console.log(personalMovieDB);
+}
+showMyDB();
